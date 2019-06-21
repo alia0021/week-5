c775a4da17774c2f561e4937ee68d2b1f5955d70
Frah <alia0021@algonquinlive.com>
2019-06-21 12:20:41 -0400

    initial commit

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <slot name="title"></slot>
+    <!-- slot is a placeholder for my todoform (component) -->
+    <form @submit.prevent="addTodo">
+      <!-- prevent prevents the submit to send it to another page so prevents stops the action from happening -->
+      <input type="text" v-model="newTodo">
+    </form>
+    <slot name="desc"></slot>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      newTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.length) {
+        this.$emit("newTodo", this.newTodo); // emit is a child to the parent prop. u use $emit to send it to the parent
+        this.newTodo = "";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+input {
+  mind-width: 200px;
+  padding: 10px 20px;
+  font-size: 1.5rem;
+}
+</style>
---
+++ src/views/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <TodoForm @newTodo="addTodo">
+      <h2 slot="title">Add a Todo</h2>
+      <p slot="desc">Your todos are saved</p>
+      <p></p>
+    </TodoForm>
+    <!-- slot needs a pair tag to con -->
+
+    <Todo :todos="todoList" @removeTodo="appDeleteTodo"/>
+  </div>
+</template>
+<script>
+import Todo from "@/components/Todo.vue";
+import TodoForm from "@/components/TodoForm.vue";
+export default {
+  data() {
+    return {
+      todoList: ["Walk the dogs", "Go for a ride"]
+    };
+  },
+  components: {
+    Todo,
+    TodoForm
+  },
+  methods: {
+    appDeleteTodo(index) {
+      this.todoList.splice(index, 1);
+    },
+    addTodo(todo) {
+      this.todoList.push(todo);
+    }
+  }
+};
+</script>
+<style scoped>
+ul {
+  list-style: none;
+  width: 50%;
+  margin: 0 auto;
+}
+ul li {
+  border-bottom: 1px solid #acacac;
+  padding: 10px 0;
+  margin-bottom: 10px;
+}
+</style>
